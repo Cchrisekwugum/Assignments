0ce5fc0f2c3bc97d1d7547c7c5a5ad2f58d20b5d
Cchrisekwugum <cchrisekwugum@gmail.com>
2025-09-14 12:55:46 +0100

    fix path issues

--- version2.py
+++ version2.py
@@ -2,27 +2,9 @@ import pandas as pd
 import json
 import os
 
-
-USER_data = "users.json"
-
-if os.path.exists(USER_data):
-    with open(USER_data, "r") as f:
-        users = json.load(f)
-else:
-    users = {}
-
-
-users = {int(k): v for k, v in users.items()}
-
-
-next_id = max(users.keys(), default=0) + 1
-
-import pandas as pd
-import json
-import os
-
-
-USER_data = r"Assignments/users.json"
+print(os.getcwd())
+#USER_data = "users.json"
+USER_data = r"../users.json"
 
 if os.path.exists(USER_data):
     with open(USER_data, "r") as f:
